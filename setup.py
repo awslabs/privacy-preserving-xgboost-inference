752287b5edbf706ca5eadd4fef1058978ea6e454
Eric Crockett <ericcro@amazon.com>
2023-03-30 16:06:18 -0700

    Moved requirements to pyproject.toml and removed requirements.txt, setup.py, and setup.cfg

--- setup.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-# https://ianhopkinson.org.uk/2022/02/understanding-setup-py-setup-cfg-and-pyproject-toml-in-python/
-
-from setuptools import setup
-
-if __name__ == "__main__":
-    setup()
